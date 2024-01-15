87cfb93606f4726ae05edf21cb7dfcbc6456f649
Albert Yann <albertyann@gmail.com>
2024-01-15 21:03:32 +0800

    初始化代码

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # tools-kit
 这里放一些网络上收集到的，自己用的工具
+
+## 常用工具箱
+该项目原子 [issues](https://github.com/CorentinTh/it-tools) 
+
+基于此，做一些自己常用的工具，会保持同步。
+
+## License
+
+This project is under the [GNU GPLv3](LICENSE).
---
+++ src/components/NavbarButtons.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+import { BrandGithub, BrandTwitter, InfoCircle, Moon, Sun } from '@vicons/tabler';
+import { useStyleStore } from '@/stores/style.store';
+
+const styleStore = useStyleStore();
+const { isDarkTheme } = toRefs(styleStore);
+</script>
+
+<template>
+  <!-- <c-tooltip :tooltip="$t('home.nav.github')" position="bottom">
+    <c-button
+      circle
+      variant="text"
+      href="https://github.com/CorentinTh/it-tools"
+      target="_blank"
+      rel="noopener noreferrer"
+      :aria-label="$t('home.nav.githubRepository')"
+    >
+      <n-icon size="25" :component="BrandGithub" />
+    </c-button>
+  </c-tooltip> -->
+
+  <c-tooltip :tooltip="$t('home.nav.twitter')" position="bottom">
+    <c-button
+      circle
+      variant="text"
+      href="https://twitter.com/albertyann"
+      rel="noopener"
+      target="_blank"
+      :aria-label="$t('home.nav.twitterAccount')"
+    >
+      <n-icon size="25" :component="BrandTwitter" />
+    </c-button>
+  </c-tooltip>
+
+  <!-- <c-tooltip :tooltip="$t('home.nav.about')" position="bottom">
+    <c-button circle variant="text" to="/about" :aria-label="$t('home.nav.aboutLabel')">
+      <n-icon size="25" :component="InfoCircle" />
+    </c-button>
+  </c-tooltip> -->
+</template>
+
+<style lang="less" scoped>
+.n-button {
+  &:not(:last-child) {
+    margin-right: 5px;
+  }
+}
+</style>
